2447631ff2a80bb5637faa848de3cbad18747d69
Mershana <102474997+Mershana@users.noreply.github.com>
2022-09-28 15:39:31 +0300

    06. Установка времени в объекте Date

---
+++ 06. Установка времени в объекте Date/js/app.js
@@ -0,0 +1,9 @@
+'use strict'
+
+// Установка времени в объекте Date в JavaScript
+/* №1⊗jsPmTmTS
+Узнайте, какой день недели был в ваш день рождения. */
+/* let date = new Date('1998-03-16');
+let day = date.getDay();
+let days = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
+console.log(days[day]); */
