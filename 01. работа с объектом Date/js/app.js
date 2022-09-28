878b21c85ae0375ca5af927f178802ec593bef15
Mershana <102474997+Mershana@users.noreply.github.com>
2022-09-28 15:18:55 +0300

    01. работа с объектом Date

---
+++ 01. работа с объектом Date/js/app.js
@@ -0,0 +1,29 @@
+'use strict'
+
+// Работа с объектом Date в JavaScript
+// let date = new Date();
+
+/* let date = new Date();
+
+console.log(date.getFullYear()); // год
+console.log(date.getMonth());    // месяц
+console.log(date.getDate());     // день
+
+console.log(date.getHours());    // часы
+console.log(date.getMinutes());  // минуты
+console.log(date.getSeconds());  // секунды
+Учтите, что месяц, который возвращает метод getMonth, начинается с нуля - январь нулевой, февраль первый и так далее. */
+
+/* №1⊗jsPmTmOD
+Выведите на экран текущий день. */
+/* console.log(date.getDate());  *///9
+
+/* №2⊗jsPmTmOD
+Выведите на экран текущий месяц. */
+/* let date = new Date();
+console.log(date.getMonth()); */ //7
+
+/* №3⊗jsPmTmOD
+Выведите на экран текущий год. */
+let date = new Date();
+console.log(date.getFullYear()); 
