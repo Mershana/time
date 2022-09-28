d025a1031ee7f124eefacaa52ab98c3fa124dd9f
Mershana <102474997+Mershana@users.noreply.github.com>
2022-09-28 15:20:36 +0300

    04. Получения дня недели

---
+++ 04. Получения дня недели/js/app.js
@@ -0,0 +1,32 @@
+'use strict'
+
+// Получения дня недели на JavaScript
+
+/* №1⊗jsPmTmWG
+Выведите на экран номер текущего дня недели. */
+/* let date = new Date();
+console.log(date.getDay()); */ //2
+
+/* №2⊗jsPmTmWG
+Определите, является ли текущий день недели выходным или рабочим днем. */
+/* let date = new Date();
+let today = date.getDay();
+
+if (today >= 1 && today <= 5){
+	console.log('сегодня рабочий день');
+} else {
+	console.log('сегодня выходной день')
+} */
+
+/* №3⊗jsPmTmWG
+Определите сколько дней осталось до ближайшего воскресенья. */
+/* let date = new Date
+let day = 6 - date.getDay(); //4
+console.log(day); */
+
+/* if (day === 6 && day === 0){
+	console.log(6 - date.getDate() + ' до выходных');
+} else if (day === 5 || day === 1){
+	console.log(6 - date.getDate() + ' до выходных');
+}
+ */
