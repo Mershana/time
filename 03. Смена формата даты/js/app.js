ddfcd9c6274715626e8a47d2857870bdaa8a828f
Mershana <102474997+Mershana@users.noreply.github.com>
2022-09-28 15:20:15 +0300

    03. Смена формата даты

---
+++ 03. Смена формата даты/js/app.js
@@ -0,0 +1,14 @@
+'use strict'
+
+/* №1⊗jsPmTmFCh
+Дана дата в формате год-месяц-день. Преобразуйте эту дату в формат день.месяц.год. */
+/* let str = '2022-08-09'
+let res = str.split('-').reverse().join('.');
+
+console.log(res); */
+
+//через обращение к ключам 
+/* let str = '2022-08-09';
+let arr = str.split('-');
+let res = arr[2] + '.' + arr[1] + '.' + arr[0];
+console.log(res); */
