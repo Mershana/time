5ae99dc6c9d4185e6678f1acc5f39792abae2298
Mershana <102474997+Mershana@users.noreply.github.com>
2022-09-30 19:41:48 +0300

    08. Разница между объектами с датой в JavaScript

---
+++ 08. Разница между объектами с датой в JavaScript/js/app.js
@@ -0,0 +1,21 @@
+'use strict'
+
+// Разность между датами в формате timestamp в JavaScript
+/* №1⊗jsPmTmTmD
+Выведите на экран количество дней, прошедшее между 1 марта 1988 года и 10 января 2000 года. */
+/* let now = new Date('1998-03-01');
+let date = new Date('2000-01-10');
+
+let diff = now.getTime() - date.getTime();
+let watch = diff / (1000 * 60 * 60)
+console.log(watch / 24);  */// 680
+
+/* №2⊗jsPmTmTmD
+Выведите на экран количество месяцев, прошедшее между вашим рождением и текущим моментом времени. */
+/* let now = new Date('1998-03-16');
+let date = new Date();
+
+let diff = now.getTime() - date.getTime()
+let watch = diff / (1000 * 60 * 60);
+let month = watch / 730;
+console.log(month); */
