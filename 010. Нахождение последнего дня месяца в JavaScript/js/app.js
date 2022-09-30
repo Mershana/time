83d2453d421620cdded90cd773edfc059ea8a460
Mershana <102474997+Mershana@users.noreply.github.com>
2022-09-30 19:42:17 +0300

    010. Нахождение последнего дня месяца в JavaScript

---
+++ 010. Нахождение последнего дня месяца в JavaScript/js/app.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/* №1⊗jsPmTmMLD
+Оформите этот способ решения проблемы в виде функции, которая параметрами будет принимать месяц и год и возвращать номер последнего дня этого месяца. */
+/* function getLastDayOfMonth(year, month){
+	let date = new Date(year, month, 0);
+	return date.getDate();
+}
+console.log(getLastDayOfMonth(1998, 3)); //31
+console.log(getLastDayOfMonth(2022, 8)); //31
+console.log(getLastDayOfMonth(1950, 2)); */ //28
+
+/* №2⊗jsPmTmMLD
+Определите, какой день недели будет в последнем дне пятого месяца 2025 года. */
+/* let days = new Date('2025-05-30');
+let dayOfTheWeek;
+let Day = [ "воскресенье", "понедельник", "вторник", "среда",
+            "четверг", "пятница", "суббота" ];
+dayOfTheWeek =  Day[days.getDay()];
+document.write(dayOfTheWeek);  */ //пятница
