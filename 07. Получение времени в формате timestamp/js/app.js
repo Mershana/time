afbe58fca824ce13617897f14a8fa75ca92a8610
Mershana <102474997+Mershana@users.noreply.github.com>
2022-09-30 19:41:31 +0300

    07. Получение времени в формате timestamp

---
+++ 07. Получение времени в формате timestamp/js/app.js
@@ -0,0 +1,9 @@
+'use strict'
+
+// Получение времени в формате timestamp в JavaScript
+
+/* №1⊗jsPmTmTmF
+Выведите на экран timestamp, соответствующий дате 1 января 2025 года. */
+/* let date = new Date(2025, 1);
+console.log(date.getTime()); */
+
