cfc80c2c1aecd22a6d14857cdba4c979f745e343
Mershana <102474997+Mershana@users.noreply.github.com>
2022-09-30 19:42:32 +0300

    011. Определение високосного года в JavaScript

---
+++ 011. Определение високосного года в JavaScript/js/app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+// Определение високосного года в JavaScript
+
+/* №1⊗jsPmTmLE
+Сделайте функцию isLeap, которая параметром будет принимать год и возвращать true, если этого год високосный, и false - если нет. */
+/* function isLeap(year){
+ 	year = prompt('Введите год');
+	if (year % 4 != 0 || (year % 100 == 0) && year % 400 != 0 ){
+		return false
+	} else {
+		return true;
+	}
+}
+console.log(isLeap()); */
+
